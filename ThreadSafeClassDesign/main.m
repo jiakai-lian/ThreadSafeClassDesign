b69f68ec5951bd49be6cc71cab58a7d2b947f6b1
Jiakai Lian <jiakai.lian@gmail.com>
2015-11-05 17:03:38 +1100

    Xcode project set up

---
+++ ThreadSafeClassDesign/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ThreadSafeClassDesign
+//
+//  Created by bluecats on 5/11/2015.
+//  Copyright © 2015 jiakai. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
